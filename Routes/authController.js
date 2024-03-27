c188c5d6b411d7b31b065062b8d34221a4efcaf5
unnath123 <unnathpgowda@gmail.com>
2024-03-27 18:11:00 +0530

    rollback

--- Routes/authController.js
+++ Routes/authController.js
@@ -40,10 +40,10 @@ authRoute.post("/register", async (req, res)=>{
 
 })
 
-authRoute.get("/login", async(req, res)=>{
-    // const {loginID, password} = req.body;
-    const loginID = "test2"
-    const password = "456";
+authRoute.post("/login", async(req, res)=>{
+    const {loginID, password} = req.body;
+    // const loginID = "test2"
+    // const password = "456";
 
     console.log("hi there")
     if(!loginID || !password){
