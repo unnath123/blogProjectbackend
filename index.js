380d5a11a664ef5b86a5f48052b641e6f473bfa7
unnath123 <unnathpgowda@gmail.com>
2024-03-28 23:09:16 +0530

    kk

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(session({
     resave: false,
     store:store,
     cookie: {
-      domain: 'blogprojectbackend.onrender.com', // Set domain to localhost
+      domain: 'https://blogprojectbackend.onrender.com', // Set domain to localhost
       // path:"/",
       maxAge: 1000*60*60*24*2,
       secure: true,
